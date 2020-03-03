3b34eac9277f0823473e14fc06b6f1a541765cf8
PassCody <44557819+PassCody@users.noreply.github.com>
2020-03-03 11:15:12 +0100

    Update Menue.java

--- OOP/src/uebung_I/Menue.java
+++ OOP/src/uebung_I/Menue.java
@@ -5,37 +5,35 @@ import java.util.Scanner;
 public class Menue {
 	
 	public Menue(Scanner sc, double a, double b) {
-		System.out.println("Was möchten Sie mit den Zahlen machen?");
+		System.out.println("Was mÃ¶chten Sie mit den Zahlen machen?");
 		System.out.println("1. Addition");
 		System.out.println("2. Subtraktion");
 		System.out.println("3. Multiplikation");
 		System.out.println("4. Divisdieren");
 		SetResault sr;
 		double resault;
-		double[] myarray = new double[2];
-		myarray[0] = a;
-		myarray[1] = b;
+		double[] myarray = {a, b};
 		switch (sc.nextInt()){
 			case 1:
-				sr = new SetResault(a, b, 1);
+				sr = new SetResault(myarray, 1);
 				resault = sr.get_resault_addition();
 				System.out.println("Das Ergebniss lautet: " + resault);
 				new Menue(sc, myarray);
 			break;
 			case 2:
-				sr = new SetResault(a, b, 2);
+				sr = new SetResault(myarray, 2);
 				resault = sr.get_resault_subtraktion();
 				System.out.println("Das Ergebniss lautet: " + resault);
 				new Menue(sc, myarray);
 			break;
 			case 3:
-				sr = new SetResault(a, b, 3);
+				sr = new SetResault(myarray, 3);
 				resault = sr.get_resault_multiplikation();
 				System.out.println("Das Ergebniss lautet: " + resault);
 				new Menue(sc, myarray);
 			break;
 			case 4:
-				sr = new SetResault(a, b, 4);
+				sr = new SetResault(myarray, 4);
 				resault = sr.get_resault_divisdieren();
 				System.out.println("Das Ergebniss lautet: " + resault);
 				new Menue(sc, myarray);
@@ -46,15 +44,13 @@ public class Menue {
 	}
 
 	public Menue(Scanner sc, double[] myarray) {
-		System.out.println("Was möchten Sie nun machen?");
+		System.out.println("Was mÃ¶chten Sie nun machen?");
 		System.out.println("1. Andere Rechnart");
 		System.out.println("2. Neue Zahlen");
 		System.out.println("99. Programm Beenden");
-		double a = myarray[0];
-		double b = myarray[1];
 		switch (sc.nextInt()){
 		case 1:
-			new Menue(sc, a, b);
+			new Menue(sc, myarray[0], myarray[1]);
 		break;
 		case 2:
 			uebungs_Main.main(null);
